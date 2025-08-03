a9aca7adf4539b108886626f1ed9a954422aef63
Todd Riemenschneider <twr14152@gmail.com>
2025-08-03 11:38:30 -0400

    Update add_remove_device.py

--- network_db/add_remove_device.py
+++ network_db/add_remove_device.py
@@ -13,23 +13,31 @@ ios_rtr = {
 }
 
 
-conn = sqlite3.connect("netdb.sql")
+conn = sqlite3.connect("netwk.db")
 cursor = conn.cursor()
 
+print("Gathering device data......")
 
 net_connect = ConnectHandler(**ios_rtr)
 hostname = net_connect.send_command("show hostname")
 config = net_connect.send_command('show running-config')
 mgmt_ip = net_connect.send_command('show interface mgmt 0  | inc "Internet Address"')
 os_version = net_connect.send_command('show version | inc "NXOS: version"')
-print("hostname:")
-print(hostname)
-print("**************")
-print("os version:")
-print(os_version)
-print("**************")
-print("Config: ")
-print(config)
+
+
+print("Getting hostname:")
+#print(hostname)
+print("-----------------")
+print("Getting os version:")
+#print(os_version)
+print("-----------------")
+print("Getting mgmt_ip:")
+#print(mgmt_ip)
+print("-----------------")
+print("Getting config: ")
+#print(config)
+print("Gathering data complete....")
+
 
 device = {
     "hostname": hostname, 
@@ -39,6 +47,7 @@ device = {
 }
 
 def add_device(conn, **device):
+    print("Adding data to the database...")
     try:
         cursor.execute("""
         INSERT INTO devices (hostname, os_version, mgmt_ip, config) 
@@ -50,6 +59,7 @@ def add_device(conn, **device):
         print(f"Error adding device: {e}")
 
 def update_device(conn, **device):
+    print("Updating data in the database...")
     try:
         cursor = conn.cursor()
         cursor.execute("""
@@ -66,6 +76,7 @@ def update_device(conn, **device):
         print(f"Error update failed: {e}")
 
 def validate_device(conn, hostname):
+    print("Reading data in the database...")
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM devices WHERE hostname = ?", (hostname,))
     row = cursor.fetchone()
@@ -73,6 +84,7 @@ def validate_device(conn, hostname):
 
 
 def delete_device(conn, hostname):
+    print("Deleting data from the database...")
     cursor = conn.cursor()
     cursor.execute("DELETE FROM devices WHERE hostname = ?", (hostname,))
     conn.commit()

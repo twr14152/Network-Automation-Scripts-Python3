89c8fbfa17e342892331a31ffe391667e49983af
Todd Riemenschneider <twr14152@gmail.com>
2025-10-19 10:06:46 -0400

    Add lab_script.py for executing commands on devices
    arista ceos lab script

---
+++ EAPI/lab_script.py
@@ -0,0 +1,30 @@
+import requests
+import json
+from pprint import pprint as pp
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+host_list = input("Enter device to connect to: ")
+hosts = [i.strip() for i in host_list.split(",")] #splits into a list and removes white space
+
+commands = input('Enter commands to run seperated by ",": ')
+cmds = commands.split(",")
+
+for host in hosts:
+    url = "https://" + host + "/command-api"
+    print(f"Connected to {host}..")
+    headers = {'Content-Type': 'application/json'}
+    payload = {
+            "jsonrpc": "2.0",
+            "method": "runCmds",
+            "params": {
+                "version": 1,
+                "cmds": cmds,
+                "format": "json"
+                },
+            "id": "1"
+            }
+    response = requests.post(url, data=json.dumps(payload), auth=('admin', 'admin'), verify=False)
+    pp(response.json())
+    print(f"Closing connection to {host}...\n\n")

7faae0e5f0a10e2b86fcd004c27ea15aeb817830
Todd Riemenschneider <twr14152@gmail.com>
2025-10-23 14:53:40 -0400

    Enable command execution on multiple hosts
    Modify script to accept multiple hosts and execute commands on each. More of an apples to apples comparison with lab_script.py

--- eAPI/lab_script_2.py
+++ eAPI/lab_script_2.py
@@ -1,12 +1,17 @@
 from jsonrpclib import Server
-import json
 import ssl
- 
+import json
+
 ssl._create_default_https_context = ssl._create_unverified_context
+
+host_list = input("Enter host or hosts separated by ',': ")
+hosts = [i.strip() for i in host_list.split(",")] 
 
 commands = input("Enter commands: ")
 cmds = commands.split(",")
 
-switch = Server( 'https://admin:admin@clab-lab3-ceos1/command-api' )
-response = switch.runCmds( 1, cmds )
-print(json.dumps(response, indent=2))
+for host in hosts:
+    switch = Server(f"https://admin:admin@clab-lab3-{host}/command-api")
+    response = switch.runCmds( 1, cmds )
+    print(json.dumps(response, indent=2))
+

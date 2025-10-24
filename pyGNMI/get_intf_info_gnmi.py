c1217f65ee38c41c6b08fbb5cbea3670f2ea28d8
Todd Riemenschneider <twr14152@gmail.com>
2025-10-24 07:24:33 -0400

    Add gNMI interface information retrieval script

---
+++ pyGNMI/get_intf_info_gnmi.py
@@ -0,0 +1,48 @@
+from pygnmi.client import gNMIclient
+import json
+
+HOST = "clab-lab3-ceos1"
+PORT = 6030
+USERNAME = "admin"
+PASSWORD = "admin"
+
+
+interface = input("Enter target interface (example Ethernet1):")
+intf_settings = input("What do you want to look at (config, state, counters): ")
+
+'''
+Paths for reference
+/interfaces
+/interfaces/interface[name]/config
+/interfaces/interface[name]/state
+/interfaces/interface[name]/subinterfaces/subinterface[index]/config
+/interfaces/interface[name]/subinterfaces/subinterface[index]/state
+/interfaces/interface[name]/state/counters
+'''
+
+config_path = f"/interfaces/interface[name={interface}]/config"
+state_path = f"/interfaces/interface[name={interface}]/state"
+counters_path = f"/interfaces/interface[name={interface}]/state/counters"
+
+
+def main():
+    target = (HOST, PORT)
+    with gNMIclient(target=target, username=USERNAME, password=PASSWORD, insecure=True) as gc:
+        try:
+            if intf_settings == "config":
+                result = gc.get(path=[config_path], encoding="json_ietf")
+                print("Interface configuration:")
+                print(json.dumps(result, indent=2))
+            elif intf_settings == "state":
+                result = gc.get(path=[state_path], encoding="json_ietf")
+                print("Interface status:")
+                print(json.dumps(result, indent=2))
+            elif intf_settings == "counters":
+                result = gc.get(path=[counters_path], encoding="json_ietf")
+                print("Interface counters:")
+                print(json.dumps(result, indent=2))
+        except Exception as e:
+            print("Error performing gNMI get:", e)
+
+if __name__ == "__main__":
+    main()

bd63a5f34b08f4cd70943ddf249e3ab4899050bc
Todd Riemenschneider <twr14152@gmail.com>
2025-11-30 08:57:04 -0500

    Add JSON-RPC script for OSPF configuration

---
+++ srlinux/json_rpc/add_ospf_sw2.py
@@ -0,0 +1,68 @@
+import requests
+import json
+
+
+url = "http://clab-lab1-sw2/jsonrpc"
+username = "admin"
+password = "NokiaSrl1!"
+
+
+set_commands = [
+    # set ospf version
+    {
+        "action": "update",
+        "path": "/network-instance[name=default]/protocols/ospf/instance[name=default]/version",
+        "value": "ospf-v2"
+    },
+    # set ospf router id
+    {
+        "action": "update",
+        "path": "/network-instance[name=default]/protocols/ospf/instance[name=default]/router-id",
+        "value": "2.2.2.2"
+    },
+    # add interface to ospf
+    {
+        "action": "update",
+        "path": "/network-instance[name=default]/protocols/ospf/instance[name=default]/area[area-id=0.0.0.0]/interface[interface-name=ethernet-1/1.0]",
+        "value": {}
+    },
+    # set interface to be point-to-point network in ospf
+    {
+        "action": "update",
+        "path": "/network-instance[name=default]/protocols/ospf/instance[name=default]/area[area-id=0.0.0.0]/interface[interface-name=ethernet-1/1.0]/interface-type",
+        "value": "point-to-point"
+    },
+    # enable interface in ospf
+    {
+        "action": "update",
+        "path": "/network-instance[name=default]/protocols/ospf/instance[name=default]/area[area-id=0.0.0.0]/interface[interface-name=ethernet-1/1.0]/admin-state",
+        "value": "enable"
+    }
+]
+
+
+payload = {
+    "jsonrpc": "2.0",
+    "method": "set",
+    "params": {
+        "commands": set_commands,
+        "output-format": "json"  # or "text" if you prefer
+    },
+    "id": 1
+}
+
+try:
+    response = requests.post(
+            url,
+            data=json.dumps(payload),
+            auth=(username, password),
+            headers={"Content-Type": "application/json"},
+            verify=False,
+            timeout=5
+    )
+    print(response.status_code)
+    print(json.dumps(response.json(), indent=2))
+except requests.exceptions.RequestException as e:
+    print("Error: ", e)
+
+
